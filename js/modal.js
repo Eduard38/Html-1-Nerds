5e4cb7c8e6d97469e88875c91dd56f7145b80eac
Eduard38 <kamaev-eduard@yandex.ru>
2022-09-25 13:21:25 +0500

    В скрипт modal.js добавил: - закрытие по Esc - localstorage у формы - фокус на инпуте при открытии формы - запрет отправки формы без заполненых полей

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,43 @@
-var modal = document.querySelector('.modal');
-var modalButtonOpen = document.querySelector('.contacts-button');
-var modalButtonClose = document.querySelector('.button-close');
+const modal = document.querySelector('.modal');
+const form = modal.querySelector('form');
+const modalButtonOpen = document.querySelector('.contacts-button');
+const modalButtonClose = document.querySelector('.button-close');
+const login = modal.querySelector('[name=login]');
+const mail = modal.querySelector('[name=mail]');
+var isStorageSupport = true;
+const storage = '';
+
+try {
+    storage = localStorage.getItem('login');
+} catch(err) {
+    isStorageSupport = false;
+}
+
 modalButtonOpen.addEventListener('click', function(evt) {
     evt.preventDefault();
     modal.classList.add('modal-show');
+    if (storage) {
+        login.value = storage;
+        password.focus();
+    } else {
+        login.focus();
+    }
 });
+
 modalButtonClose.addEventListener('click', function() {
     modal.classList.remove('modal-show');
 });
 
-// document.addEventListener('click', function() {
-//     modal.classList.remove('modal-show');
-// });
+form.addEventListener('submit', function(evt) {
+    if (!login.value || !password.value) {
+        evt.preventDefault();
+    } else {
+        localStorage.setItem('login', login.value);
+    }
+});
+window.addEventListener('keydown', function(evt) {
+    if (evt.key === 'Escape') {
+        modal.classList.remove('modal-show');
+    }
+});
+
